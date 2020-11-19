7bcecce96b854f5be7ea8f118b50c8d4ed4500e1
오주담 <dhwnkea@naver.com>
2020-11-19 15:17:16 +0900

    [WPF MvvM] Command BInding Basic 1

---
+++ WPFCommandsDemo/WPFCommandsDemo/Command.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFCommandsDemo
+{
+    public class Command : ICommand
+    {
+        Action<object> _excuteMethod;
+        Func<object, bool> _canExcuteMethod;
+        public event EventHandler CanExecuteChanged;
+
+        public Command(Action<object> excuteMethod, Func<object, bool> canExecuteMethod)
+        {
+            this._excuteMethod = excuteMethod;
+            this._canExcuteMethod = canExecuteMethod;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _excuteMethod(parameter);
+        }
+    }
+}
---
+++ WPFCommandsDemo/WPFCommandsDemo/ViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace WPFCommandsDemo
+{
+    public class ViewModel
+    {
+        public ICommand MyCommand { get; set; }
+
+        public ViewModel()
+        {
+            MyCommand = new Command(ExcuteMethod, CanExecuteMethod);
+        }
+
+        private bool CanExecuteMethod(object arg)
+        {
+            return true;
+        }
+
+        private void ExcuteMethod(object obj)
+        {
+            MessageBox.Show("코드비하인드가 아닌 Command ExecuteMethod");
+        }
+    }
+}
