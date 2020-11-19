4902f2cda0bb76989661eae1adbc6a4f4808e05a
오주담 <dhwnkea@naver.com>
2020-11-19 13:49:00 +0900

    init commit

---
+++ WPFMvvMTest/WPFMvvMTest/ViewModel/MainViewModel.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Input;
+
+namespace WPFMvvMTest.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private int iNum;
+
+        public int Number
+        {
+            get { return iNum; }
+            set
+            {
+                iNum = value;
+
+                OnPropertyChanged("Number");
+                OnPropertyChanged("PlusEnable");
+                OnPropertyChanged("MinusEnable");
+
+                PageContents = string.Format("{0} 페이지를 보고 있어요.", iNum);
+            }
+        }
+
+        public MainViewModel()
+        {
+            Number = 1;
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private ICommand minusCommand;
+
+        public ICommand MinusCommand
+        {
+            get { return (this.minusCommand) ?? (this.minusCommand = new DelegateCommand(Minus)); }
+        }
+
+        private void Minus()
+        {
+            Number--;
+        }
+
+        public bool MinusEnable
+        {
+            get { return Number > 1 ? true : false; }
+        }
+        private ICommand plusCommand;
+
+        public ICommand PlusCommand
+        {
+            get { return (this.plusCommand) ?? (this.plusCommand = new DelegateCommand(Plus)); }
+        }
+
+        private void Plus()
+        {
+            Number++;
+        }
+
+        public bool PlusEnable
+        {
+            get { return Number < 10 ? true : false; }
+        }
+        private string pageContent;
+        public string PageContents
+        {
+            get { return pageContent; }
+            set
+            {
+                pageContent = value;
+                OnPropertyChanged("PageContents");
+            }
+        }
+    }
+
+    public class DelegateCommand : ICommand
+    {
+        private readonly Func<bool> canExecute;
+        private readonly Action execute;
+
+        public DelegateCommand(Action execute) : this(execute, null)
+        {
+
+        }
+
+        public DelegateCommand(Action execute, Func<bool> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object o)
+        {
+            if (this.canExecute == null)
+            {
+                return true;
+            }
+            return this.canExecute();
+        }
+
+        public void Execute(object o)
+        {
+            this.execute();
+        }
+
+        public void RaiseCanExecuteChanged()
+        {
+            if (this.CanExecuteChanged != null)
+            {
+                this.CanExecuteChanged(this, EventArgs.Empty);
+            }
+        }
+    }
+}
