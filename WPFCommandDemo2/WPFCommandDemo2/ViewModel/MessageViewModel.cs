9e2ae6a5c8c901ac140fba1ed2e3a5fa48e15db5
오주담 <dhwnkea@naver.com>
2020-11-19 15:48:25 +0900

    [WPF MvvM] Command Binding Basic2

---
+++ WPFCommandDemo2/WPFCommandDemo2/ViewModel/MessageViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPFCommandDemo2.ViewModel.Command;
+
+namespace WPFCommandDemo2.ViewModel
+{
+    public class MessageViewModel
+    {
+        //public string MessageText { get; set; }
+        public MessageCommand DisplayMessageCommand { get; private set; }
+
+        public MessageViewModel()
+        {
+            DisplayMessageCommand = new MessageCommand(DisplayMessage);
+        }
+
+        public void DisplayMessage(string message)
+        {
+            MessageBox.Show(message);
+        }
+    }
+}
